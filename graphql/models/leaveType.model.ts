95f9d2a99bc5aa876130a7ca84bcf2fecd8ce9a8
Munkhtmln <munkhtemuulen0809@gmail.com>
2025-06-23 20:42:46 +0800

    s

--- graphql/models/leaveType.model.ts
+++ graphql/models/leaveType.model.ts
@@ -1,10 +1,16 @@
 import { model, models, Schema } from "mongoose";
 
 const leaveTypeSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
   casualLeave: { type: Number },
   paidLeave: { type: Number },
   annualLeave: { type: Number },
   remoteWork: { type: Number },
+
+  lastMonthlyRefresh: { type: Date },
+  lastAnnualRefresh: { type: Date },
 });
+
 export const leaveTypeModel =
   models.leaveType || model("leaveType", leaveTypeSchema);
